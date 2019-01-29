44a569aabd8eb295cfbad9282b4578229ef1149d
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-01-29 14:31:58 -0500

    Update supply_chain.test.js

--- test/supply_chain.test.js
+++ test/supply_chain.test.js
@@ -1,12 +1,10 @@
 /*
-
 This test file has been updated for Truffle version 5.0. If your tests are failing, make sure that you are
 using Truffle version 5.0. You can check this by running "trufffle version"  in the terminal. If version 5 is not
 installed, you can uninstall the existing version with `npm uninstall -g truffle` and install the latest version (5.0)
 with `npm install -g truffle`.
-
 */
-var BN = web3.utils.BN
+
 var SupplyChain = artifacts.require('SupplyChain')
 
 contract('SupplyChain', function(accounts) {
@@ -66,8 +64,8 @@ contract('SupplyChain', function(accounts) {
         assert.equal(result[3].toString(10), 1, 'the state of the item should be "Sold", which should be declared second in the State Enum')
         assert.equal(result[5], bob, 'the buyer address should be set bob when he purchases an item')
         assert.equal(eventEmitted, true, 'adding an item should emit a Sold event')
-        assert.equal(new BN(aliceBalanceAfter).toString(), new BN(aliceBalanceBefore).add(new BN(price)).toString(), "alice's balance should be increased by the price of the item")
-        assert.isBelow(Number(bobBalanceAfter), Number(new BN(bobBalanceBefore).sub(new BN(price))), "bob's balance should be reduced by more than the price of the item (including gas costs)")
+        assert.equal(parseInt(aliceBalanceAfter), parseInt(aliceBalanceBefore, 10) + parseInt(price, 10), "alice's balance should be increased by the price of the item")
+        assert.isBelow(parseInt(bobBalanceAfter), parseInt(bobBalanceBefore, 10) - parseInt(price, 10), "bob's balance should be reduced by more than the price of the item (including gas costs)")
     })
 
     it("should allow the seller to mark the item as shipped", async() => {
